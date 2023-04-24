a9e06bba133e6fb7a510f1f4144cecb17ac4a49d
Clinton <clfray@cisco.com>
2023-04-24 17:42:02 +0200

    initial extension

---
+++ CPUMonitor/cpu.bat
@@ -0,0 +1,3 @@
+@echo off
+
+Powershell.exe -ExecutionPolicy remotesigned -File cpu.ps1
---
+++ CPUMonitor/cpu.ps1
@@ -0,0 +1,7 @@
+﻿Out-Null
+
+$nc = (Get-CIMInstance -Class 'CIM_Processor').NumberOfCores
+$nl = (Get-CIMInstance -Class 'CIM_Processor').NumberOfLogicalProcessors
+
+write-host("name=Hardware Resources|CPU|NumberOfCores,aggregator=OBSERVATION,value=$nc")
+write-host("name=Hardware Resources|CPU|NumberOfLogicalProcessors,aggregator=OBSERVATION,value=$nl")
